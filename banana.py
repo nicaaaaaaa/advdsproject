54e998fc71af5c56aa0e8722bab93df4e0f94766
nicaaaaaaa <93868117+nicaaaaaaa@users.noreply.github.com>
2025-01-15 21:13:40 +0800

    Update banana.py

--- banana.py
+++ banana.py
@@ -380,11 +380,11 @@ st.markdown("""
 
 """)
 with st.expander("References"):
-    st.markdown(""
+    st.markdown("""
     - **Article**:
       - [Agrimag. (n.d.). Harga pisang berangan meningkat. Retrieved February 20, 2024 from, https://agrimag.my/bm/article-details/harga-pisang-berangan-meningkat]
       - [https://manamurah.com/barang/pisang_berangan-18/negeri/perak-8]
-      "")
+      """)
 
 
 
