e32ecc7d769b149e7fbea1b831443994a2c48a57
nicaaaaaaa <93868117+nicaaaaaaa@users.noreply.github.com>
2025-01-15 21:22:37 +0800

    Update banana.py

--- banana.py
+++ banana.py
@@ -248,7 +248,7 @@ district_predictions_perak = {
 district_price_perak = pd.DataFrame({
     'district': ['Muallim', 'Perak Tengah', 'Kerian', 'Kinta', 'Hulu Perak', 
                  'Manjung', 'Kuala Kangsar', 'Larut, Matang & Selama', 
-                 'Hilir Perak', 'Batang Padang',Bagan Datuk],
+                 'Hilir Perak', 'Batang Padang','Bagan Datuk'],
     'item_price': [6.08, 6.30, 6.5, 6.89, 6.72, 6.30, 6.21, 6.34, 7.46, 6.30,40]
 })
 
